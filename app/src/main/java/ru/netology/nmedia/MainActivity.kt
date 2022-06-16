e8a04ff3e2460c99871642ee9964af30b6cca298
Nikita Semushin <semushin.nik@inbox.ru>
2022-06-16 17:01:24 +0500

    Home_work-5

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,9 +1,10 @@
 package ru.netology.nmedia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.netology.nmedia.data.Post
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,36 +12,29 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-            author = "Нетология. Университет интернет-профессий будущего",
-            published = "21 мая в 18:36",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-            likedCount = 999,
-            sharedCount = 9998,
-            viewedCount = 999
-        )
+        val viewModel: PostViewModel by viewModels()
 
         with(binding) {
-            author.text = post.author
-            published.text = post.published
-            content.text = post.content
-            likedCount.text = convertCountToString(post.likedCount)
-            sharedCount.text = convertCountToString(post.sharedCount)
-            viewedCount.text = convertCountToString(post.viewedCount)
-            liked.setOnClickListener {
-                if (!post.likedByMe) {
+            viewModel.data.observe(this@MainActivity) { post ->
+                author.text = post.author
+                published.text = post.published
+                content.text = post.content
+                likedCount.text = convertCountToString(post.likedCount)
+                sharedCount.text = convertCountToString(post.sharedCount)
+                viewedCount.text = convertCountToString(post.viewedCount)
+                if (post.likedByMe) {
                     liked.setImageResource(R.drawable.ic_baseline_favorite_24)
-                    post.likedCount++
                 } else {
                     liked.setImageResource(R.drawable.ic_baseline_favorite_border_24)
-                    post.likedCount--
                 }
-                post.likedByMe = !post.likedByMe
-                likedCount.text = convertCountToString(post.likedCount)
             }
+
+            liked.setOnClickListener {
+                viewModel.like()
+            }
+
             shared.setOnClickListener {
-                post.sharedCount++
-                sharedCount.text = convertCountToString(post.sharedCount)
+                viewModel.share()
             }
         }
     }
